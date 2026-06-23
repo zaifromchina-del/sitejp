5031fccef11c18492eaca6c733e8a4247e27c9dc
joaoss4 <jojomoringaduos@gmail.com>
2026-06-23 17:20:37 -0300

    alteraçao nome e layout

--- static/script.js
+++ static/script.js
@@ -1,6 +1,6 @@
 // scripts.js
 document.addEventListener("DOMContentLoaded", () => {
-    console.log("SiteJP carregado!");
+    console.log("Previsão de Demanda carregado!");
 
     // Exemplo: animar campos no futuro
     // ou validar formulário antes do submit
